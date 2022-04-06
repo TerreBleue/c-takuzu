2f9335017d2baaa264088a7f4161d6769470fcf8
Ma2t-Br <94540875+Ma2t-Br@users.noreply.github.com>
2022-04-06 16:48:16 +0200

    fichiers functions

---
+++ functions.c
@@ -0,0 +1,11 @@
+//
+// Created by matth_uaxpdbk on 06/04/2022.
+//
+
+#include "functions.h"
+
+BOOL good_colomne (int** Grille, int* Colomne, int Taille) {
+    if (Taille == 4) {
+
+    }
+}
---
+++ functions.h
@@ -0,0 +1,10 @@
+//
+// Created by matth_uaxpdbk on 06/04/2022.
+//
+
+#ifndef PROJET_TAKUZU_FUNCTIONS_H
+#define PROJET_TAKUZU_FUNCTIONS_H
+
+typedef enum {true, False} BOOL;
+
+#endif //PROJET_TAKUZU_FUNCTIONS_H
