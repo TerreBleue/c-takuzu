6ee3cbc9fa512c7e5feb119b0c0f33e5269c320f
inesfh <100560608+inesfh@users.noreply.github.com>
2022-04-20 12:37:24 +0200

    bool ligne_est_binaire

--- functions.c
+++ functions.c
@@ -1,6 +1,8 @@
 #include "functions.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include "mat.h"
 
 bool good_colonne(int **Grille, int *colonne, int Taille) {
@@ -13,10 +15,20 @@ char ** creer_masque(int Taille) {
     for (int i = 0; i < Taille; i++) {
         masque[i] = calloc(Taille, sizeof(char));
     }
-    char *ligne_masque;
+    char * ligne_masque=calloc(Taille, sizeof(char));
+    bool ligne_est_binaire;
     for (int i = 0; i < Taille; i++) {
         printf("Entrer masque[%d]", i);
-        scanf("%s", ligne_masque);
-
+        do {
+            scanf("%s", ligne_masque);
+            ligne_est_binaire=true;
+            for(int j=0;j<strlen(ligne_masque);j++){
+                if(ligne_masque[j]!='1' && ligne_masque[j]!='0'){
+                    continue;
+                }
+                ligne_est_binaire=false;
+            }
+        }while(strlen(ligne_masque)!= Taille && ligne_est_binaire == false);
+        masque[i]= ligne_masque;
     }
 }
