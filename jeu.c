b737e60630709e5678d4a7147dc1bd706d42d458
Terreclair <82955208+TerreBleue@users.noreply.github.com>
2022-04-23 16:34:44 +0200

    saisie sécurisée

--- jeu.c
+++ jeu.c
@@ -12,8 +12,19 @@ bool grille_completee(char** masque) {
 
 void jouer(int** tab, char** masque, int vies) {
     afficher_grille(tab, masque);
-    bool est_correct;
-    while (vies > 0 && grille_completee(masque) == false) { // jeu
 
+    int taille = (int) strlen(masque[0]);
+    bool est_correct;
+    char emplacement[2] = "";
+    int val_entree;
+
+    while (vies > 0 && grille_completee(masque) == false) { // jeu
+        printf("emplacement (ligne colonne) et valeur =");
+        do { scanf("%2s %1d", emplacement, &val_entree); }
+        while ('1' <= emplacement[0] && emplacement[0] <= '1' + taille - 1
+               && 'A' <= emplacement[1] && emplacement[1] <= 'A' + taille - 1
+               && (val_entree == 0 || val_entree == 1)
+               && masque[(int) (emplacement[0] - '1')][(int) (emplacement[1] - 'A')] == '1');
+        // comparer avec grille solution
     }
 }
