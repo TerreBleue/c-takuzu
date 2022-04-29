cf0571aefaac1f09132c767286448654667b5bdb
Terreclair <82955208+TerreBleue@users.noreply.github.com>
2022-04-29 19:20:02 +0200

    écriture du menu
    Signed-off-by: Terreclair <82955208+TerreBleue@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,28 +1,12 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <time.h>
 
-#include "functions.h"
 #include "menu.h"
-#include "mat.h"
-#include "jeu.h"
 
-
-#define VIES 3
 
 int main(void) {
     srand((unsigned int) time(NULL));
 
-    int** mat = static_to_dynamic(m4, SIZE);
-
-    char** dyn_masque = masque_aleatoire(SIZE);
-    jouer(mat, dyn_masque, VIES);
-
-    // libération finale des éléments dynamiques
-    for (int i = 0; i < SIZE; ++i) free(dyn_masque[i]);
-    free(dyn_masque);
-    dyn_masque = NULL;
-    release_mat(&mat, SIZE);
+    menu();
     return 0;
 }
--- menu.c
+++ menu.c
@@ -15,11 +15,35 @@ int m8[8][8] = {0, 0, 0, 0, 1, 1, 1, 1,
                 1, 1, 1, 0, 0, 0, 0, 1};
 
 void menu() {
-    int choix;
-    printf("1- Saisir manuellement un masque\n"
-           "2- Générer automatiquement un masque\n"
-           "3- Jouer\n"
-           "Choisir l'entier correspondant.");
-    scanf("%d", &choix);
-    // code
+    int choix = -1, ** sol = static_to_dynamic(m4, SIZE);
+    char** masque = NULL;
+    while (choix != 0) {
+        printf("0- Quitter le menu\n"
+               "1- Saisir manuellement un masque\n"
+               "2- Générer automatiquement un masque\n"
+               "3- Jouer\n"
+               "Choisir l'entier correspondant.\n");
+        do scanf("%d", &choix); while (choix < -1 || choix > 4);
+
+        switch (choix) {
+            case 1:
+                masque = masque_manuel(SIZE);
+                afficher_grille_masque(sol, masque);
+                release_masque(&masque);
+                break;
+            case 2:
+                masque = masque_aleatoire(SIZE);
+                afficher_grille_masque(sol, masque);
+                release_masque(&masque);
+                break;
+            case 3:
+                masque = masque_aleatoire(SIZE);
+                jouer(sol, masque, VIES);
+                release_masque(&masque);
+            default:
+                break;
+        }
+    }
+
+    release_mat(&sol, SIZE);
 }
