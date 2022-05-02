bd3e57b9985223cdb8e0894bbd05b4d6b01b9893
Terreclair <82955208+TerreBleue@users.noreply.github.com>
2022-05-02 20:47:00 +0200

    move fcts d'unicité vers generation.h
    Signed-off-by: Terreclair <82955208+TerreBleue@users.noreply.github.com>

---
+++ generation.h
@@ -0,0 +1,16 @@
+#ifndef PROJET_TAKUZU_GENERATION_H
+#define PROJET_TAKUZU_GENERATION_H
+#include "functions.h"
+#include "resolution.h"
+
+int modifier_derniere_valeur(char **modifs, char *str);
+int ecrire_modif(char **modifs, char **grille, int code, int i1, int i2, char input);
+int unicite_solution(char** sol, char** masque, char** grille) {
+    if (grille_completee(grille) == true) {
+        return grille_correcte(grille);
+    }
+    // TODO unicité de la solution
+    return 1;
+}
+
+#endif //PROJET_TAKUZU_GENERATION_H
