0e568903aa84e14f33bc75fb0a38e1605424f51c
Terreclair <82955208+TerreBleue@users.noreply.github.com>
2022-05-14 23:22:37 +0200

    fix saisie coup
    Signed-off-by: Terreclair <82955208+TerreBleue@users.noreply.github.com>

--- jeu.c
+++ jeu.c
@@ -2,11 +2,11 @@
 
 
 void jouer(char **sol, char **masque, int vies) {
+    int taille = size(sol);
     INDICE indice;
-    int taille = size(sol), scanf_res;
     char **grille_jeu = creer_masque(taille);
     int entree_lig, i_lig, i_col, code;
-    char val_entree, entree_col;
+    char entree_empl[3], *entree_col, val_entree;
     actualiser_grille_jeu(sol, masque, grille_jeu);
 
     printf("Entrer l'emplacement (indices ligne et colonne) et la valeur sous la forme : lignecolonne valeur\n");
@@ -14,16 +14,16 @@ void jouer(char **sol, char **masque, int vies) {
         afficher_grille(grille_jeu);
 
         do {
-            printf("empl et val=");
-            scanf_res = scanf("%2d%c %c", &entree_lig, &entree_col, &val_entree);
-            if (scanf_res == 0) scanf("%*s");
-            i_lig = entree_lig - 1, i_col = (int) (entree_col - 'A');
+            printf("ligcol et val=");
+            scanf("%3s %c", entree_empl, &val_entree);
+            entree_lig = strtol(entree_empl, &entree_col, 10); // début du str converti en int
+            i_lig = entree_lig - 1, i_col = (int) (entree_col[0] - 'A');
         } while (i_lig < 0 || i_lig >= taille
                  || i_col < 0 || i_col >= taille
                  || val_entree != '0' && val_entree != '1'
                  || grille_jeu[i_lig][i_col] != INCONNUE); // valeur déjà affichée
 
-        grille_jeu[i_lig][i_col] = val_entree; // test de la validité du coup : on met temp la grille à la valeur entrée
+        grille_jeu[i_lig][i_col] = val_entree; // test validité du coup : on met temp la grille à valeur entrée
         code = grille_correcte(grille_jeu);
         grille_jeu[i_lig][i_col] = INCONNUE;
         if (code == 1) {
@@ -35,8 +35,9 @@ void jouer(char **sol, char **masque, int vies) {
             printf("Coup invalide... Il vous reste %d vie(s).\n", --vies);
             if (code == -1) {
                 printf("Il doit y avoir autant de 0 que de 1 dans les lignes et les colonnes de la grille.\n");
-            } else if (code == -2) { printf("Il ne doit pas y avoir plus de 2 chiffres identiques côte à côte.\n"); }
-            else { printf("2 lignes ou 2 colonnes ne peuvent être identiques.\n"); } // code -3
+            } else if (code == -2) {
+                printf("Il ne doit pas y avoir plus de 2 chiffres identiques côte à côte.\n");
+            } else { printf("2 lignes ou 2 colonnes ne peuvent être identiques.\n"); } // code -3
             printf("\n");
 
             donner_indice(&indice, grille_jeu);
